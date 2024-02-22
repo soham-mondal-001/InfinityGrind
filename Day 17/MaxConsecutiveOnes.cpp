75ae7d85bc5ab14cc4d813a2b165fac14abfdda8
Soham Mondal <amisoham765@gmail.com>
2024-02-22 23:44:14 +0530

    Day 17 first

---
+++ Day 17/MaxConsecutiveOnes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0,ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==1)
+            count++;
+            else
+            count=0;
+            if(count>ans)
+            ans=count;
+        }
+        return ans;
+    }
+};
