bc3a99d6ee57631d729a4f4a6c395fb4f975a6f3
Soham Mondal <amisoham765@gmail.com>
2024-03-03 06:56:21 +0530

    Day 25 first code

---
+++ Day 25/RomanToInteger.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans=0;
+        map<char,int> mp;
+        mp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        for(int i=0;i<s.length();i++)
+        {
+            if(i+1<s.length()&&mp[s[i]]<mp[s[i+1]])
+            {
+                ans+=(mp[s[i+1]]-mp[s[i]]);
+                i++;
+            }
+            else
+                ans+=mp[s[i]];
+        }
+        return ans;
+    }
+};
