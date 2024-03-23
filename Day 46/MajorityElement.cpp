41a2fdbe9412e3d0ae87f5338f4c4e433add3d02
Soham Mondal <amisoham765@gmail.com>
2024-03-23 21:28:02 +0530

    Day 46

---
+++ Day 46/MajorityElement.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count=0,ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(count==0)
+            ans=nums[i];
+            if(ans==nums[i])
+            count++;
+            else
+            count--;
+        }
+        return ans;
+    }
+};
